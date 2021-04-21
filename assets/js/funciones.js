1260531f0375022e9b23a8d4679f5022dd1b4f2b
Hector Ccollatupa <hccollatupa@gmail.com>
2021-04-21 17:20:57 -0500

    Retorno de las funciones

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -3,8 +3,12 @@ todas las funciones tradicionales (No sucede con las funciones flecha)
 manejan de manera implícita la variable "arguments"
 */
 function saludar(nombre) {
-    console.log(arguments);
-    console.log('Hola ' + nombre);
+    //console.log(arguments);
+    //console.log('Hola ' + nombre);
+    return [1, 2, 3];
+
+    //Esto nunca se va a ejecutar
+    console.log("Soy un código que está después del return");
 }
 
 /*
@@ -34,8 +38,39 @@ const saludarFlecha2 = (nombre) => {
 si no le pasamos el parámetro, por defecto toda variable no inicializada
 toma el valor "undefined"
 */
-saludar('Hector', 40, true, 'Costa Rica');
+const retornoDeSaludar = saludar('Hector', 40, true, 'Costa Rica');
+//console.log(retornoDeSaludar[0], retornoDeSaludar[1]);
 //saludar2('Hector');
 
-saludarFlecha();
-saludarFlecha2('Hector');
+//saludarFlecha();
+//saludarFlecha2('Hector');
+
+/*
+toda función en javascript si no tiene explícitamente la palabra
+reservada "return", por defecto retorna "undefined".
+*/
+
+function sumar(a, b) {
+    return a + b;
+}
+
+/*
+const sumar2 = (a, b) => {
+    return a + b;
+}
+*/
+
+/*
+una particularidad de las funciones de flecha es que no necesitan
+la palabra reservada "return".
+*/
+const sumar2 = (a, b) => a + b;
+//console.log(sumar2(2, 2));
+
+function getAleatorio() {
+    return Math.random();
+}
+//console.log(getAleatorio());
+
+const getAleatorio2 = () => Math.random();
+console.log(getAleatorio2());

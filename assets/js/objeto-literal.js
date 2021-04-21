4faac18edb51c2d11bc148779b38b136da0565b3
Hector Ccollatupa <hccollatupa@gmail.com>
2021-04-21 15:39:31 -0500

    Más detalles sobre los objetos literales

--- assets/js/objeto-literal.js
+++ assets/js/objeto-literal.js
@@ -1,4 +1,4 @@
-let personaje = {
+const personaje = {
     nombre: 'Tony Stark',
     codeName: 'Ironman',
     vivo: false,
@@ -31,3 +31,36 @@ const x = 'vivo';
 console.log('Vivo', personaje[x])
 
 console.log('Última película', personaje['ultima-pelicula']);
+
+// Más detalles
+delete personaje.edad;
+
+console.log(personaje);
+
+personaje.casado = true;
+
+const entriesPares = Object.entries(personaje);
+console.log(entriesPares);
+
+/*
+La variable "personaje" ha sido definida como const, por lo tanto, no es posible realizar asignación directa
+pero sí es posible cambiar sus propiedades.
+*/
+//personaje = true;
+
+console.log(personaje);
+
+/*
+"Object.freeze" congela o bloquea el objeto para evitar cambiar o añadir propiedades al objeto.
+Sin embargo, las propiedades complejas sí son posibles modificarlas, por ejemplo, las propiedades
+de la dirección que existe dentro de la variable "personaje", es decir "ubicación" y "zip".
+*/
+Object.freeze(personaje);
+personaje.dinero = 1000000000; //No se reflejará el cambio
+personaje.casado = false; //No se reflejará el cambio
+personaje.direccion.ubicacion = 'Costa Rica'; //Sí se refleja el cambio
+console.log(personaje);
+
+const propiedades = Object.getOwnPropertyNames(personaje);
+const valores = Object.values(personaje);
+console.log({ propiedades, valores });

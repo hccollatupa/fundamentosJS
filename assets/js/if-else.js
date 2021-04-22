6d14eabf5af0d1c09f19a7ddca2befa947794d70
Hector Ccollatupa <hccollatupa@gmail.com>
2021-04-21 19:19:43 -0500

    If y Else

---
+++ assets/js/if-else.js
@@ -0,0 +1,44 @@
+let a = 10;
+
+if (a >= 10) { //undefined, null, una asignación
+    console.log('A es mayor o igual a 10');
+} else {
+    console.log('A es menor a 10');
+}
+
+//console.log('Fin de programa');
+
+const hoy = new Date(); //{}
+console.log(hoy);
+let dia = hoy.getDay(); //0: Domingo, 1: Lunes, 2: Martes...
+console.log({ dia });
+
+console.log(3 === '3');
+console.log(3 == '3');
+//console.log(3 = '3'); //devuelve un error
+
+/*
+"dia = 1" se trata de una asigación, no una condición.
+para realizar condiciones nunca se debe utilizar el "=", en su lugar 
+utilizar "==".
+
+al utilizar triple igual "===" evalúa el valor y el tipado de los datos
+debe ser idéntico, por lo tanto, en este ejemplo se ejecutaría 
+el bloque else.
+*/
+if (dia === 3) {
+    console.log('Miércoles');
+} else if (dia == 4) {
+    console.log('Jueves');
+    /*
+    if (dia === 4) {
+        console.log('Jueves');
+    } else {
+        console.log('No es miércoles ni jueves');
+    }
+    */
+} else if (dia == 5) {
+    console.log('Viernes');
+} else {
+    console.log('No es miércoles, jueves o viernes');
+}

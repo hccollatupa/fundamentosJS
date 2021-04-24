c9fbfa1dd2d6f74dd30f592554d7e91dc201315c
Hector Ccollatupa <hccollatupa@gmail.com>
2021-04-23 23:08:47 -0500

    Sets y Gets

--- assets/js/classes/class.js
+++ assets/js/classes/class.js
@@ -2,6 +2,7 @@ class Persona {
     nombre = '';
     codigo = '';
     frase = '';
+    comida = '';
 
     constructor(nombre = 'Sin nombre', codigo = 'Sin código', frase = 'Sin frase') {
         this.nombre = nombre;
@@ -9,22 +10,36 @@ class Persona {
         this.frase = frase;
     }
 
+    set setComida(comida) {
+        this.comida = comida.toUpperCase();
+    }
+
+    get getComida() {
+        return console.log(`La comida favorita de ${this.nombre} es ${this.comida}`);
+    }
+
     quienSoy() {
         console.log(`Soy ${this.nombre} y mi identidad es ${this.codigo}`);
     }
 
     miFrase() {
-        this.quienSoy();
         console.log(`${this.codigo} dice: ${this.frase}`);
     }
 }
 
-const spiderman = new Persona('Peter Parker', 'Spiderman', 'Soy tu amigable vecino spiderman.');
-const ironman = new Persona('Tony Stark', 'Ironman', 'Yo soy Ironman.');
-//console.log(spiderman);
-//console.log(ironman);
 
+const ironman = new Persona('Tony Stark', 'Ironman', 'Yo soy Ironman.');
+console.log(ironman);
+
+const spiderman = new Persona('Peter Parker', 'Spiderman', 'Soy tu amigable vecino spiderman.');
 spiderman.quienSoy();
-spiderman.miFrase();
-ironman.quienSoy();
-ironman.miFrase();
+//spiderman.miFrase();
+
+spiderman.setComida = 'Pie de cereza de la tía May';
+//spiderman.comida = 'Duende verde'; //Tiene sentido porque "comida" no es una propiedad privada.
+
+//spiderman.nemesis = 'Duende Verde';//Js no arroja error por esta línea que está mal.
+
+console.log(spiderman.getComida);
+
+console.log(spiderman);

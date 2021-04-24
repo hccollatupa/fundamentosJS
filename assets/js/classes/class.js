260dfd117ccf4d037be08f8901f35e15aca8e8aa
Hector Ccollatupa <hccollatupa@gmail.com>
2021-04-23 22:53:24 -0500

    Sets y Gets

--- assets/js/classes/class.js
+++ assets/js/classes/class.js
@@ -8,9 +8,23 @@ class Persona {
         this.codigo = codigo;
         this.frase = frase;
     }
+
+    quienSoy() {
+        console.log(`Soy ${this.nombre} y mi identidad es ${this.codigo}`);
+    }
+
+    miFrase() {
+        this.quienSoy();
+        console.log(`${this.codigo} dice: ${this.frase}`);
+    }
 }
 
 const spiderman = new Persona('Peter Parker', 'Spiderman', 'Soy tu amigable vecino spiderman.');
 const ironman = new Persona('Tony Stark', 'Ironman', 'Yo soy Ironman.');
-console.log(spiderman);
-console.log(ironman);
+//console.log(spiderman);
+//console.log(ironman);
+
+spiderman.quienSoy();
+spiderman.miFrase();
+ironman.quienSoy();
+ironman.miFrase();
